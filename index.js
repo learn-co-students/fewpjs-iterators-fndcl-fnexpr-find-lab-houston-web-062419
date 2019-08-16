346d87955c3872e91f7d9f78f861e7147e517b57
Sfimadi94 <sydeffect1994@gmail.com>
2019-08-16 10:45:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+// record = [year, result]
+
+function superbowlWin(record){
+  let winningGame = record.find((game) => game.result === "W")
+  // if (winningGame) {
+  //   return winningGame.year
+  // }
+
+  return winningGame ? winningGame.year : undefined
+
+
+
 }
